b077ee36439018ea23fbb7d059e706d1caaef946
XuanNguyenCV <biggerthanusual12345@gmail.com>
2026-04-24 00:34:59 +0700

    MOVEMENTS

--- PacMen/src/pacMen/Main.java
+++ PacMen/src/pacMen/Main.java
@@ -3,23 +3,72 @@ package pacMen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.animation.AnimationTimer;
+import javafx.scene.shape.Circle;
+
+import java.util.*;
 
 public class Main extends Application {
+
+    // store keys pressed as a hashset
+    private Set<KeyCode> keysPressed = new HashSet<>();
 
     // EXAMPLE CODE FOR TESTING PURPOSES, LIKELY NOT IMPLENTED IN THE FUTURE
     @Override
     public void start(Stage stage) {
+        Circle player = new Circle(10, Color.YELLOW);
+        player.setCenterX(400);
+        player.setCenterY(300);        
+
         // Simple UI element
         Label label = new Label("Pac-Men is running!");
-
+        
         // Root layout
-        StackPane root = new StackPane();
+        Pane root = new Pane();
         root.getChildren().add(label);
+
+        root.getChildren().add(player);
 
         // Scene
         Scene scene = new Scene(root, 800, 600);
+
+        // Input Handling
+        scene.setOnKeyPressed(e -> keysPressed.add(e.getCode()));
+        scene.setOnKeyReleased(e -> keysPressed.remove(e.getCode()));
+
+
+        // Movement Handling
+        AnimationTimer gameLoop = new AnimationTimer() {
+            public void handle(long now) {
+
+                double speed = 1.5;
+
+                // Set player movement; Restrict movement to either Vertical or Horizontal (no Diagonal)
+                // Placeholder Commands, will fix accordingly to conform to 2D Arrays
+                if (keysPressed.contains(KeyCode.W) || keysPressed.contains(KeyCode.S)) {
+                    if (keysPressed.contains(KeyCode.W)) {
+                        player.setCenterY(player.getCenterY() - speed);
+                    }
+                    if (keysPressed.contains(KeyCode.S)) {
+                        player.setCenterY(player.getCenterY() + speed);
+                    }      
+                }
+                else {
+                    if (keysPressed.contains(KeyCode.A)) {
+                        player.setCenterX(player.getCenterX() - speed);
+                    }            
+                    if (keysPressed.contains(KeyCode.D)) {
+                        player.setCenterX(player.getCenterX() + speed);
+                    }
+                }   
+            }
+        };
+
+        gameLoop.start();
 
         // Stage setup
         stage.setTitle("Pac-Men");
--- PacMen/src/pacMen/core/GameLoop.java
+++ PacMen/src/pacMen/core/GameLoop.java
@@ -1,5 +1,7 @@
 package pacMen.core;
 
 public class GameLoop {
-    
+    public static void main(String[] args) {
+        
+    }
 }
